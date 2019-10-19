59b07c7ff41a5c0f83a5e0fbfa410089cef3a354
jensdesmeyter <43971861+jensdesmeyter@users.noreply.github.com>
2019-10-19 13:43:58 +0200

    Changed beforeAll

--- src/Signature.js
+++ src/Signature.js
@@ -1,16 +1,18 @@
 const nacl = require('libsodium-wrappers');
 let myVerifyingKey;
 
-beforeAll(async() => {
+(async() => {
     await nacl.ready;
     myVerifyingKey = nacl.crypto_sign_keypair();
-});
+})();
 
 exports.verifyingKey = async function(){
+	await nacl.ready;
     return myVerifyingKey.publicKey;
 }
 
 exports.sign = async function(msg){
+	await nacl.ready;
     return nacl.crypto_sign(msg, myVerifyingKey.privateKey);
 }
 
